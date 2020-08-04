dcbba7014f6f71aede65c9fc42eb224f5e3d2b41
Beiker Guillen <beiker19998@gmail.com>
2020-08-04 09:39:56 -0400

    Se elimina error de duplicado en asistencia

--- ajax/terapias/especial/addEspecial.php
+++ ajax/terapias/especial/addEspecial.php
@@ -25,7 +25,76 @@
 		if(!$result = $mysql->query($query)){
 			echo  "Error al agregar Terapia Especial",$mysql->error;
 		}else{
+			if($_POST['botones'] == 'save'){
+				$mysql->close();
+				include("../../db_connection.php");
+				// BUSCAR EL REGISTRO ANTERIOR DE TERAPIA DE CONFRONTACION
+				$q = $mysql->query("SELECT * FROM especial ORDER BY id_especial DESC LIMIT 1");
+				$row = $q->fetch_assoc();
+				$id_especial = $row['id_especial'];
+				$mysql->close();
+
+				// INSERTAR RESIDENTES PARA INICIO DE ASISTENCIA
+				include("../../db_connection.php");
+				$resi_q = $mysql->query("SELECT * FROM residentes WHERE etapa_resi <> 'REDUCADO' AND etapa_resi <> 'ABANDONO'");
+					while($r = $resi_q->fetch_assoc()){
+						$residente = $r['id_residente'];
+
+						$data = "INSERT INTO asistencia (id_especial, id_residente, status_asis) 
+										VALUE ('$id_especial', '$residente', '0')";
+						$resi_add_asis = $mysql->query($data);
+					}
+				$mysql->close();
+				// --- FIN LISTA DE RESIDENTES - INSERT
+
+				// AGREGAR ASISTENCIA DE RESIDENTES A TERAPIA
+				include("../../db_connection.php");
+				$resi_num = count($_POST['resi']);
+				$i = 0;
+				while($i < $resi_num){
+					$id_data = $_POST['resi'][$i];
+
+					$data_resi = "UPDATE asistencia SET status_asis = '1' WHERE id_residente = '$id_data' AND id_especial = '$id_especial' ";
+
+					$resultado = $mysql->query($data_resi);
+					$i++;
+				}
+				$mysql->close();
+				// FIN ASISTENCIA - ADD
+
+				if(!$resi_add_asis && !$resultado){
+					echo "Error al agregar asistenecia terapia especial ".$mysql->error;
+				}else{
+					header("Location: ../../../EditarTerapiaEspecial.php?id=".$id_residente."&tera=".$id_especial);
+				}
+		}elseif(empty($_POST['resi'])){
+			include("../../db_connection.php");
+				// BUSCAR EL REGISTRO ANTERIOR DE TERAPIA DE CONFRONTACION
+				$q = $mysql->query("SELECT * FROM especial ORDER BY id_especial DESC LIMIT 1");
+				$row = $q->fetch_assoc();
+				$id_especial = $row['id_especial'];
+				$mysql->close();
+				
+			// INSERTAR ASISTENCIA SIN GENERAR
+			include("../../db_connection.php");
+			$resi_q = $mysql->query("SELECT * FROM residentes WHERE etapa_resi <> 'REDUCADO' AND etapa_resi <> 'ABANDONO'");
+				while($r = $resi_q->fetch_assoc()){
+					$residente = $r['id_residente'];
+
+					$data = "INSERT INTO asistencia (id_especial, id_residente, status_asis) 
+									VALUE ('$id_especial', '$residente', '0')";
+					$resi_add_asis = $mysql->query($data);
+				}
+			$mysql->close();
+			// --- FIN LISTA DE RESIDENTES - INSERT - SIN GENERAR
+		}
+
+		if($_POST['botones'] == 'Guardar'){
 			header("location: ../../../ListaTEspecial.php");
+		 }elseif($_POST['botones'] == 'save'){
+			header("Location: ../../../EditarTerapiaEspecial.php?id=".$id_residente."&tera=".$id_especial);
+		 }
+			
 			
 		}
 	
