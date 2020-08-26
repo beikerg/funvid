94b417a353dd47bffb1d0cccf3d9dde2dd95610d
Beiker Guillen <beiker19998@gmail.com>
2020-08-26 13:23:50 -0400

    Se reparara boot en terapia especial

--- EditarTerapiaEspecial.php
+++ EditarTerapiaEspecial.php
@@ -364,7 +364,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             
             <!-- // FIN DEL BOX  4// -->
               
-             <center> <input type="submit" class="btn btn-primary btn-lg" name="botones" value="Guardar Cambios"></center>
+             <center> <button type="submit" class="btn btn-primary btn-lg" name="botones" value="Guardar">Guardar Cambios<buttton></center>
         </div>
             
             
