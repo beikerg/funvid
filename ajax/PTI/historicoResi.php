1b0b78135b724d2071bb05b913ad105cb95fecd1
Beiker Guillen <beiker19998@gmail.com>
2021-01-12 18:46:26 -0300

    extras

--- ajax/PTI/historicoResi.php
+++ ajax/PTI/historicoResi.php
@@ -21,7 +21,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Seleccionar reducado</h4>
+        <h4 class="modal-title" id="myModalLabel">Seleccionar Residente</h4>
       </div>
       <div class="modal-body">
         <!-- Contenido del modal -->      
@@ -56,7 +56,7 @@
             $data = "";
               include('ajax/db_connection.php');
              
-             $query = "SELECT * FROM residentes ";
+             $query = "SELECT * FROM residentes WHERE etapa_resi <> 'ABANDONO' AND etapa_resi <> 'REDUCADO' ";
 
 
        
